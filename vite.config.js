d11e5e86b01df76bcc63a9fee9967c47c97a217c
imlon <imlon.renfew@gmail.com>
2025-01-03 20:03:02 -0800

    Push only the dist folder

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: 'src', // Tells Vite to use the src folder as the root
-  build: {
-    outDir: '../dist', // Output files into the dist folder
-  },
-});
